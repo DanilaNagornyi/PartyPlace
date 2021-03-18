c8604add1d0d48a19e1ddd071307e8c0e08ea44d
Artnagornyi <artnagornyi@gmail.com>
2021-03-18 18:15:08 +0300

    redesign event style and routes form

--- app.js
+++ app.js
@@ -22,12 +22,11 @@ app.use(
 );
 
 app.set('view engine', 'hbs');
-app.set('views', path.join(process.env.PWD, 'views','user'));
+app.set('views', path.join(process.env.PWD, 'views'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(process.env.PWD, 'public')));
-
 
 app.use('/', mainRouter);
 app.use('/profile', userRouter);
--- routers/main.js
+++ routers/main.js
@@ -2,8 +2,24 @@ const { Router } = require('express');
 
 const router = Router();
 
+// главная ручка
 router.get('/', async (req, res) => {
   res.render('main');
+});
+
+// ручка регистрации
+router.get('/registration', async (req, res) => {
+  res.render('user/registr');
+});
+
+// ручка логина
+router.get('/login', async (req, res) => {
+  res.render('user/login');
+});
+
+// Форма создания события
+router.get('/createevent', async (req, res) => {
+  res.render('user/eventForm');
 });
 
 module.exports = router;
