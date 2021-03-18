e56ceecafa6618e257d6d6a5d0b271b3cb712c2a
Ivan Paponin <eldkirk@gmail.com>
2021-03-18 14:47:43 +0300

    DB version 0.2

--- db/seeds.js
+++ db/seeds.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Users = require('./models/Users');
+const Places = require('./models/Places');
 const { shortOption, mongoUrl } = require('./config');
 
 mongoose.connect(mongoUrl, shortOption, () => {
@@ -31,10 +32,32 @@ const users = [
   },
 ];
 
-async function usreSeeder() {
+const places = [
+  {
+    title: 'Macdonalds',
+    photo: 'https://png.pngtree.com/png-clipart/20190515/original/pngtree-pin-sign-location-icon-png-image_3566350.jpg',
+    description: 'Две мясных котлеты гриль, специальный соус сыр..... Ну вы поняли)',
+    raiting: 55,
+  },
+  {
+    title: 'Крошка Картошка',
+    photo: 'https://png.pngtree.com/png-clipart/20190515/original/pngtree-pin-sign-location-icon-png-image_3566350.jpg',
+    description: 'Откуда они берут такие огромные картофелины?!',
+    raiting: 7,
+  },
+];
+
+async function usersSeeder() {
   await Users.deleteMany();
   await Users.insertMany(users);
   await mongoose.disconnect();
 }
 
-usreSeeder();
+async function placesSeeder() {
+  await Places.deleteMany();
+  await Places.insertMany(places);
+  await mongoose.disconnect();
+}
+
+usersSeeder();
+placesSeeder();
