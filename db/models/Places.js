32b57fe38fb024f59299804898e30481b3ef66bf
Alexey <serebryakov.alx@gmail.com>
2021-03-19 10:44:16 +0300

    maps,forms,event DB

--- db/models/Places.js
+++ db/models/Places.js
@@ -2,12 +2,14 @@ const mongoose = require('mongoose');
 
 const placeSchema = new mongoose.Schema({
   title: { type: String, required: true },
+  linkToPlace: String,
   photo: {
     type: String,
     default:
       'https://png.pngtree.com/png-clipart/20190515/original/pngtree-pin-sign-location-icon-png-image_3566350.jpg',
   },
   description: { type: String, required: true },
+  geolocation: String,
   raiting: [Number],
 });
 
